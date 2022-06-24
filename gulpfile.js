8b861e74b533e8585d0110c48ba3320d6046fd0c
riki <r_karasawa@m.cajiya.co.jp>
2022-06-24 16:33:22 +0900

    プラグイン「gulp-rename」インストール→実装

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const {src, dest} = require('gulp');
+const rename = require('gulp-rename');
 
 function copyFiles() {
-    return src('./src/**')
+    return src('./src/**/*.html')
+        .pipe(rename({
+            prefix: 'hello-'
+        }))
         .pipe(dest('./dist'));
 }
 
